edfb6457d591f4bfbf09f420c11dcaa02866b465
Prithankur ⚡ <161138941+prithankurdasgupta30@users.noreply.github.com>
2026-09-17 00:21:02 +0530

    Day 10

---
+++ merge_sort_acc.py
@@ -0,0 +1,32 @@
+def merge(arr,l,m,r):
+        left, right = arr[l:m+1], arr[m+1:r+1]
+        i,j,k = l,0,0
+        while j < len(left) and k < len(right):
+                if left[j] <= right[k]:
+                        arr[i] = left[j]
+                        j+=1
+                else:
+                        arr[i] = right[k]
+                        k+=1
+                i+=1
+        while j<len(left):
+                arr[i] = left[j]
+                j+=1
+                i+=1
+        while k<len(right):
+                arr[i] = right[k]
+                k+=1
+                i+=1
+def mergeSort(arr,l,r):
+        if l>= r:
+                return
+        m = (l+r)//2
+        mergeSort(arr,l,m)
+        mergeSort(arr,m+1,r)
+        merge(arr,l,m,r)
+        return arr
+arr = list(map(int,input().split(",")))
+print(mergeSort(arr,0,len(arr)-1))
+
+
+        
